228261790f711855a8a88c55acc50ee8b42fbefd
Ilia Zolas <ilia@thedmcsa.co.za>
2023-05-06 15:48:55 +0200

    added sart and finish icons to timeline

--- client/src/sections/card-section.js
+++ client/src/sections/card-section.js
@@ -1,14 +1,18 @@
 import './card-section.scss';
 import Timeline from '../components/timeline';
+import Flag from "../assets/Flag.png"
+import Crown from "../assets/CrownSimple.png"
 
 const CardSection = () => {
     return (
         <div className="card-section">
             <h4>Your Conversation</h4>
             <h6>Starts here</h6>
+            <img src={Flag} alt="" style={{ paddingBottom:"40px"}} />
             <div className='timeline-container'>
                 <Timeline />
             </div>
+            <img src={Crown} alt="" style={{ paddingTop:"40px"}} />
         </div>
     );
 }
